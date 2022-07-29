7f1a21b82851bf7c72cf9a737bee8923341ce06d
Brad Lajoie <blajoie@vestmark.com>
2022-07-29 17:02:58 -0400

    restoring spring security in discovery service

--- discovery-server/src/main/java/com/baeldungtutorial/discoveryserver/security/AdminSecurityConfig.java
+++ discovery-server/src/main/java/com/baeldungtutorial/discoveryserver/security/AdminSecurityConfig.java
@@ -1,15 +1,30 @@
 package com.baeldungtutorial.discoveryserver.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class AdminSecurityConfig {
-    @Bean
+public class AdminSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and().httpBasic().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/").hasRole("ADMIN").antMatchers("/info", "/health").authenticated().anyRequest().denyAll()
+                .and().csrf().disable();
+    }
+
+    //@Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception
     {
         /*http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
--- discovery-server/src/main/java/com/baeldungtutorial/discoveryserver/security/SecurityConfig.java
+++ discovery-server/src/main/java/com/baeldungtutorial/discoveryserver/security/SecurityConfig.java
@@ -4,24 +4,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 @Order(1)
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    public void configureGlobal1(AuthenticationManagerBuilder auth) throws Exception
-    {
-        auth.inMemoryAuthentication().withUser("discUser").password("discPassword").roles("SYSTEM");
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("discUser").password("{noop}discPassword").roles("SYSTEM");
     }
 
-    @Bean
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS).and().requestMatchers().antMatchers("/eureka/**").and().authorizeRequests().antMatchers("/eureka/**").hasRole("SYSTEM").anyRequest().denyAll().and().httpBasic().and().csrf()
+                .disable();
+    }
+
+    //@Bean
     public SecurityFilterChain secFilterChain(HttpSecurity http) throws Exception
     {
         /*http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS).and()
