c02a691a2744f8ea46245182f2cbcebb9c374422
Brad Lajoie <blajoie@vestmark.com>
2022-07-29 22:56:30 -0400

    restore and fix spring security in gateway

--- gateway-server/src/main/java/com/baeldungtutorial/gatewayserver/security/SecurityConfig.java
+++ gateway-server/src/main/java/com/baeldungtutorial/gatewayserver/security/SecurityConfig.java
@@ -1,22 +1,18 @@
-/*package com.baeldungtutorial.gatewayserver.security;
+package com.baeldungtutorial.gatewayserver.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 
 //@EnableWebSecurity
-@EnableWebFluxSecurity
-@Configuration
-public class SecurityConfig {
+//@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
+    //@Autowired
     public void configureGlobal1(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.inMemoryAuthentication().withUser("user").password("password").roles("USER")
@@ -24,14 +20,40 @@ public class SecurityConfig {
                 .withUser("admin").password("admin").roles("ADMIN");
     }
 
-    @Bean
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /*http.authorizeRequests()
+                .antMatchers("/book-service/books").permitAll()
+                .antMatchers("/eureka/**").hasRole("ADMIN")
+                .anyRequest().authenticated().and()
+                .formLogin().and()
+                .httpBasic().and()
+                .logout().permitAll().and()
+                .csrf().disable();*/
+        /*http
+                //.formLogin()
+                //.defaultSuccessUrl("/home/index.html", true)
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/books/**", "/ratings/**", "/login*", "/").permitAll()
+                .antMatchers("/eureka/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .and()
+                .csrf().disable();*/
+        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+    }
+
+    //@Bean
     public SecurityFilterChain gatewayFilterChain(HttpSecurity http) throws Exception
     {
         http.authorizeRequests()
                 .antMatchers("/book-service/books").permitAll()
                 .antMatchers("/eureka/**").hasRole("ADMIN")
                 .anyRequest().authenticated().and()
-                //.formLogin().and()
+                .formLogin().and()
                 .httpBasic().and()
                 .logout().permitAll().and()
                 .csrf().disable();
@@ -39,4 +61,3 @@ public class SecurityConfig {
         return http.build();
     }
 }
-*/
---
+++ gateway-server/src/main/java/com/baeldungtutorial/gatewayserver/security/SecurityConfigFlux.java
@@ -0,0 +1,50 @@
+package com.baeldungtutorial.gatewayserver.security;
+import static org.springframework.security.authorization.AuthorityReactiveAuthorizationManager.hasRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigFlux {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .httpBasic().and()
+                .formLogin();
+        /*http
+                // ...
+                .authorizeExchange((authorize) -> authorize
+                        .pathMatchers("/books/**", "/ratings/**", "/login*", "/").permitAll()
+                        .pathMatchers("/eureka/**").hasRole("ADMIN")
+                        //.anyExchange().authenticated()
+                        //.and().httpBasic().and().formLogin()
+                        .anyExchange().denyAll()
+                );*/
+        return http.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("admin").roles("ADMIN").build();
+        return new MapReactiveUserDetailsService(user, admin);
+    }
+}
