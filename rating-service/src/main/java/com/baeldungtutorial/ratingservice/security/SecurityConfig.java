6c1453691e5a4c92c58bbc653f60c2f3686c4f8f
Brad Lajoie <blajoie@vestmark.com>
2022-07-29 23:07:43 -0400

    restore spring security on rating service

--- rating-service/src/main/java/com/baeldungtutorial/ratingservice/security/SecurityConfig.java
+++ rating-service/src/main/java/com/baeldungtutorial/ratingservice/security/SecurityConfig.java
@@ -33,19 +33,20 @@ public class SecurityConfig {//extends WebSecurityConfigurerAdapter {
 
     private HttpSecurity configureCommon(HttpSecurity http) throws Exception
     {
-        /*http.httpBasic()
+        http.httpBasic()
                 .disable()
                 .authorizeRequests()
                 .regexMatchers("^/ratings\\?bookId.*$").authenticated()
                 .antMatchers(HttpMethod.POST, "/ratings").authenticated()
                 .antMatchers(HttpMethod.PATCH, "/ratings/*").hasRole("ADMIN")
                 .antMatchers(HttpMethod.DELETE, "/ratings/*").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/ratings").hasRole("ADMIN")
+                //.antMatchers(HttpMethod.GET, "/ratings").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/ratings").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
-                .disable();*/
-        http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
+                .disable();
+        //http.authorizeRequests().anyRequest().permitAll().and().csrf().disable();
         return http;
     }
 }
