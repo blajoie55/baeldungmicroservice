86bbef9f885df6296c6a20ec70122ebce8b3ccbb
Brad Lajoie <blajoie@vestmark.com>
2022-08-01 10:36:40 -0400

    get redis working on gateway

--- gateway-server/src/main/java/com/baeldungtutorial/gatewayserver/config/SessionConfig.java
+++ gateway-server/src/main/java/com/baeldungtutorial/gatewayserver/config/SessionConfig.java
@@ -1,12 +1,20 @@
-/*package com.baeldungtutorial.gatewayserver.config;
+package com.baeldungtutorial.gatewayserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.session.FlushMode;
+import org.springframework.session.data.redis.RedisIndexedSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
 @EnableRedisHttpSession(flushMode = FlushMode.IMMEDIATE)
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+
+    @Bean
+    public RedisIndexedSessionRepository redisIndexedSessionRepository(RedisTemplate<Object, Object> redisTemplate)
+    {
+        return new RedisIndexedSessionRepository(redisTemplate);
+    }
 }
-*/
